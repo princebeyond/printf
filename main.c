c562c1c226064e26b54cc4d3d2c2433b7862b70c
princebeyond <amoodeyond222@gmail.com>
2023-10-14 08:54:13 -0700

    ll

--- main.c
+++ main.c
@@ -1,4 +1,39 @@
 #include "main.h"
+#include <stddef.h>
+/**
+ * print_binary - print binary
+ * @args: arguements
+ *
+ * Return: success
+ */
+int print_binary(va_list b)
+{
+	int count, mask;
+	unsigned int num;
+
+	num = va_arg(b, unsigned int);
+
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+
+	count = 0;
+	mask = 1U << (sizeof(unsigned int) * 8 - 1);
+
+	while (mask > 0)
+	{
+		if (num & mask)
+			_putchar('1');
+		else
+			_putchar('0');
+		mask >>= 1;
+		count++;
+	}
+
+	return (count);
+}
 
 /**
  * main - Entry point
