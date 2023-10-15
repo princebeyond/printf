4c6cf15ade00f0f7a4fa16af40d0f3badba2c1fa
princebeyond <amoodeyond222@gmail.com>
2023-10-15 07:53:40 -0700

    ll

--- _printf.c
+++ _printf.c
@@ -15,6 +15,7 @@ int (*find_function(const char *format))(va_list)
 		{"s", print_string},
 		{"i", print_int},
 		{"d", print_dec},
+		{"b", print_binary},
 		{NULL, NULL}
 	};
 
--- print_binary.c
+++ print_binary.c
@@ -5,12 +5,12 @@
  *
  * Return: integer
  */
-int print_binary(va_list val)
+int print_binary(va_list b)
 {
 	int flag = 0;
 	int count = 0;
-	int i, a = 1, b;
-	unsigned int num  = va_arg(val, unsigned int);
+	int i, a = 1, k;
+	unsigned int num  = va_arg(b, unsigned int);
 	unsigned int p;
 
 	for (i = 0; i < 32; i++)
@@ -20,8 +20,8 @@ int print_binary(va_list val)
 			flag = 1;
 		if (flag)
 		{
-			b = p >> (31 - i);
-			_putchar(b + 48);
+			k = p >> (31 - i);
+			_putchar(k + 48);
 			count++;
 		}
 	}
