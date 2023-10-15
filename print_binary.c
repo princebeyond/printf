82eee06ebcab2c3ee3f5141a6c4f1dbdc2e3340a
princebeyond <amoodeyond222@gmail.com>
2023-10-15 10:01:12 -0700

    ll

--- print_binary.c
+++ print_binary.c
@@ -1,34 +1,27 @@
-#include "main.h"
 /**
- * print_binary - converts to binary
- * @b: argument
+ * print_binary - converts an unsigned int to binary
+ * @val: argument
  *
- * Return: integer
+ * Return: the number of characters printed
  */
 int print_binary(va_list b)
 {
-	int flag = 0;
-	int count = 0;
-	int i, a = 1, k;
-	unsigned int num  = va_arg(b, unsigned int);
-	unsigned int p;
+    unsigned int num = va_arg(b, unsigned int);
+    int count = 0;
+    int i;
 
-	for (i = 0; i < 32; i++)
-	{
-		p = ((a << (32 - i)) & num);
-		if (p >> (31 - i))
-			flag = 1;
-		if (flag)
-		{
-			k = p >> (31 - i);
-			_putchar(k + 48);
-			count++;
-		}
-	}
-	if (count == 0)
-	{
-		count++;
-		_putchar('0');
-	}
-	return (count);
+    if (num == 0)
+    {
+        _putchar('0');
+        return (1);
+    }
+
+    for (i = 31; i >= 0; i--)
+    {
+        int bit = (num >> i) & 1;
+        _putchar(bit + '0');
+        count++;
+    }
+
+    return count;
 }
