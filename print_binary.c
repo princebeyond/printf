991c4e92b2d9cbc038198bc473807e9f09095eca
princebeyond <amoodeyond222@gmail.com>
2023-10-15 10:04:42 -0700

    ll

--- print_binary.c
+++ print_binary.c
@@ -6,22 +6,23 @@
  */
 int print_binary(va_list b)
 {
-    unsigned int num = va_arg(b, unsigned int);
-    int count = 0;
-    int i;
+	unsigned int num = va_arg(b, unsigned int);
+	int count = 0;
+	int i;
+	int bit;
 
-    if (num == 0)
-    {
-        _putchar('0');
-        return (1);
-    }
+	if (num == 0)
+	{
+		_putchar('0');
+	return (1);
+	}
 
-    for (i = 31; i >= 0; i--)
-    {
-        int bit = (num >> i) & 1;
-        _putchar(bit + '0');
-        count++;
-    }
+	for (i = 31; i >= 0; i--)
+	{
+		bit = (num >> i) & 1;
+		_putchar(bit + '0');
+		count++;
+	}
 
-    return count;
+	return (count);
 }
